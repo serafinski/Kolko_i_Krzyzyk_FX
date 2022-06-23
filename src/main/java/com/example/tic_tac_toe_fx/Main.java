f933bda8076991ad5555ea631451064112d50e72
Tomasz <tomek.serafinski@gmail.com>
2022-06-24 01:00:11 +0200

    Pseudo działający numpad (potrzebuje debbuga)

--- src/main/java/com/example/tic_tac_toe_fx/Main.java
+++ src/main/java/com/example/tic_tac_toe_fx/Main.java
@@ -1,24 +1,41 @@
 package com.example.tic_tac_toe_fx;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("title_view.fxml")));
-        Group group = new Group(root);
-        Scene scene = new Scene(group,575,600);
+        //Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("title_view.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("basicgame.fxml"));
+        Parent root = loader.load();
+        TicTacToeController controller = loader.getController();
+        Scene scene = new Scene(root,575,600);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case NUMPAD7 -> controller.klawisze(7);
+                    case NUMPAD8 -> controller.klawisze(8);
+                    case NUMPAD9 -> controller.klawisze(9);
+                    case NUMPAD4 -> controller.klawisze(4);
+                    case NUMPAD5 -> controller.klawisze(5);
+                    case NUMPAD6 -> controller.klawisze(6);
+                    case NUMPAD1 -> controller.klawisze(1);
+                    case NUMPAD2 -> controller.klawisze(2);
+                    case NUMPAD3 -> controller.klawisze(3);
+                }
+            }
+        });
 
         stage.setTitle("Kółko i krzyżyk!");
 
--- src/main/java/com/example/tic_tac_toe_fx/TicTacToeController.java
+++ src/main/java/com/example/tic_tac_toe_fx/TicTacToeController.java
@@ -82,6 +82,27 @@ public class TicTacToeController {
 
     private int ilerund = 0;
 
+    private boolean klikniety7;
+
+    private boolean klikniety8;
+
+    private boolean klikniety9;
+
+    private boolean klikniety4;
+
+    private boolean klikniety5;
+
+    private boolean klikniety6;
+
+    private boolean klikniety1;
+
+    private boolean klikniety2;
+
+    private boolean klikniety3;
+
+    private boolean koniecgry;
+
+    ArrayList<Boolean> stany;
     ArrayList<Button> przyciski;
 
     public void ustalSymbol(Button przycisk){
@@ -110,6 +131,7 @@ public class TicTacToeController {
         przyciskPowrotu.setVisible(false);
         niePokazujLinii();
         przyciski = new ArrayList<>(Arrays.asList(przycisk1,przycisk2,przycisk3,przycisk4,przycisk5,przycisk6,przycisk7,przycisk8,przycisk9));
+        stany = new ArrayList<>(Arrays.asList(klikniety1,klikniety2,klikniety3,klikniety4,klikniety5,klikniety6,klikniety7,klikniety8,klikniety9));
 
         przyciski.forEach(przycisk -> {
             ustawPrzycisk(przycisk);
@@ -132,18 +154,22 @@ public class TicTacToeController {
             };
 
             if(linia.equals("XXX")){
+                koniecgry = true;
                 przyciski.forEach(przycisk -> {
-                    ustawPrzycisk(przycisk);
                     przycisk.setDisable(true);
                 });
+                stany.forEach(stan ->{
+                    stan = true;
+                });
+
                 rysujLinie(i);
                 tekst.setText("Wygrał krzyżyk!");
                 przyciskPowrotu.setVisible(true);
             }
             else
                 if(linia.equals("OOO")){
+                    koniecgry = true;
                     przyciski.forEach(przycisk -> {
-                        ustawPrzycisk(przycisk);
                         przycisk.setDisable(true);
                     });
                     rysujLinie(i);
@@ -179,5 +205,99 @@ public class TicTacToeController {
             case 7 -> linia7.setVisible(true);
             case 8 -> linia8.setVisible(true);
         }
+    }
+    public void klawisze(int i){
+        switch (i) {
+            case 7 -> {
+                if(!klikniety7){
+                    ustalSymbol(przycisk7);
+                    przycisk7.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety7 = true;
+            }
+            case 8 -> {
+                if(!klikniety8){
+                    ustalSymbol(przycisk8);
+                    przycisk8.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety8 = true;
+            }
+            case 9 -> {
+                if(!klikniety9){
+                    ustalSymbol(przycisk9);
+                    przycisk9.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety9 = true;
+            }
+            case 4 -> {
+                if(!klikniety4){
+                    ustalSymbol(przycisk4);
+                    przycisk4.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety4 = true;
+            }
+            case 5 -> {
+                if(!klikniety5){
+                    ustalSymbol(przycisk5);
+                    przycisk5.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety5 = true;
+            }
+            case 6 -> {
+                if(!klikniety6){
+                    ustalSymbol(przycisk6);
+                    przycisk6.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety6 = true;
+            }
+            case 1 -> {
+                if(!klikniety1){
+                    ustalSymbol(przycisk1);
+                    przycisk1.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety1 = true;
+
+                if(koniecgry){
+                    przycisk1.setDisable(true);
+                }
+            }
+            case 2 -> {
+                if(!klikniety2){
+                    ustalSymbol(przycisk2);
+                    przycisk2.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety2 = true;
+
+                if(koniecgry){
+                    przycisk2.setDisable(true);
+                }
+            }
+            case 3 -> {
+                if(!klikniety3){
+                    ustalSymbol(przycisk3);
+                    przycisk3.setDisable(true);
+                    ilerund++;
+                    czyKoniec();
+                }
+                klikniety3 = true;
+            }
+        }
+
     }
 }
