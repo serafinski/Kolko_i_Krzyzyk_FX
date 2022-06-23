8ec6e933a4a961e2278399ac7e8b35cd05431df9
Tomasz <tomek.serafinski@gmail.com>
2022-06-23 17:05:04 +0200

    Edits

--- src/main/java/com/example/tic_tac_toe_fx/Main.java
+++ src/main/java/com/example/tic_tac_toe_fx/Main.java
@@ -15,14 +15,11 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("title_view.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 500, 500);
 
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("title_view.fxml")));
         Group group = new Group(root);
         Scene scene = new Scene(group,600,600, Color.BLACK);
 
-        stage.getIcons().add(new Image("D:\\PJATK\\Semestr_2\\GUI\\Tic_Tac_Toe_FX\\src\\main\\resources\\graphics\\logo.jpg"));
         stage.setTitle("Ultimate Tic-Tac-Toe!");
 
         stage.setScene(scene);
