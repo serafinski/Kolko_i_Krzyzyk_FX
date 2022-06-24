a7c686d3559191a0ecfb94c1f58ac1c37be423c5
Tomasz <tomek.serafinski@gmail.com>
2022-06-24 02:25:38 +0200

    Kijowe, ale działające rozwiązanie

--- src/main/java/com/example/tic_tac_toe_fx/TicTacToeController.java
+++ src/main/java/com/example/tic_tac_toe_fx/TicTacToeController.java
@@ -1,20 +1,11 @@
 package com.example.tic_tac_toe_fx;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Line;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
 
 public class TicTacToeController {
     @FXML
@@ -102,13 +93,72 @@ public class TicTacToeController {
         }
     }
 
-    public void ustawPrzycisk(Button przycisk){
-        przycisk.setOnMouseClicked(mouseEvent -> {
-            ustalSymbol(przycisk);
-            przycisk.setDisable(true);
-            ilerund++;
-            czyKoniec();
-        });
+    public void ustawPrzycisk(int i){
+        switch (i) {
+            case 7 -> przycisk7.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk7);
+                przycisk7.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety7 = true;
+            });
+            case 8 -> przycisk8.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk8);
+                przycisk8.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety8 = true;
+            });
+            case 9 -> przycisk9.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk9);
+                przycisk9.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety9 = true;
+            });
+            case 4 -> przycisk4.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk4);
+                przycisk4.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety4 = true;
+            });
+            case 5 -> przycisk5.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk5);
+                przycisk5.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety5 = true;
+            });
+            case 6 -> przycisk6.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk6);
+                przycisk6.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety6 = true;
+            });
+            case 1 -> przycisk1.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk1);
+                przycisk1.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety1 = true;
+            });
+            case 2 -> przycisk2.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk2);
+                przycisk2.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety2 = true;
+            });
+            case 3 -> przycisk3.setOnMouseClicked(mouseEvent -> {
+                ustalSymbol(przycisk3);
+                przycisk3.setDisable(true);
+                ilerund++;
+                czyKoniec();
+                klikniety3 = true;
+            });
+        }
     }
 
     public void initialize(){
@@ -116,8 +166,10 @@ public class TicTacToeController {
         przyciski = new ArrayList<>(Arrays.asList(przycisk1,przycisk2,przycisk3,przycisk4,przycisk5,przycisk6,przycisk7,przycisk8,przycisk9));
         stany = new ArrayList<>(Arrays.asList(klikniety1,klikniety2,klikniety3,klikniety4,klikniety5,klikniety6,klikniety7,klikniety8,klikniety9));
 
+        for (int i=1; i<= przyciski.size(); i++)
+            ustawPrzycisk(i);
+
         przyciski.forEach(przycisk -> {
-            ustawPrzycisk(przycisk);
             przycisk.setFocusTraversable(false);
         });
     }
@@ -142,6 +194,16 @@ public class TicTacToeController {
                     przycisk.setDisable(true);
                 });
 
+                klikniety1 = true;
+                klikniety2 = true;
+                klikniety3 = true;
+                klikniety4 = true;
+                klikniety5 = true;
+                klikniety6 = true;
+                klikniety7 = true;
+                klikniety8 = true;
+                klikniety9 = true;
+
                 rysujLinie(i);
                 tekst.setText("Wygrał krzyżyk!");
             }
@@ -151,6 +213,17 @@ public class TicTacToeController {
                     przyciski.forEach(przycisk -> {
                         przycisk.setDisable(true);
                     });
+
+                    klikniety1 = true;
+                    klikniety2 = true;
+                    klikniety3 = true;
+                    klikniety4 = true;
+                    klikniety5 = true;
+                    klikniety6 = true;
+                    klikniety7 = true;
+                    klikniety8 = true;
+                    klikniety9 = true;
+
                     rysujLinie(i);
                     tekst.setText("Wygrało kółko!");
             }
